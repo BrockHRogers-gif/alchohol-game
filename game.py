2baecbdcbd47f00cf3c5a240d9d1ea9e2f549225
BrockHRogers-gif <BrockHRogers@icloud.com>
2026-09-16 17:52:05 +0930

    game.py

--- game.py
+++ game.py
@@ -1,5 +1,3 @@
-Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
 print('Welcome to the alcohol consequence game. Press A, then enter to start')
 
 command = input('')
